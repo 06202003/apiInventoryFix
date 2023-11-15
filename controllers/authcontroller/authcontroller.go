490fcbfae6d8c1cc59b85cb7dc5abb24480f9d58
Yehezkiel David Setiawan <yehezkieldavid2006@gmail.com>
2023-11-15 19:54:12 +0700

    last touch???

--- controllers/authcontroller/authcontroller.go
+++ controllers/authcontroller/authcontroller.go
@@ -7,13 +7,79 @@ import (
 
 	"github.com/06202003/apiInventory/config"
 
-	"github.com/golang-jwt/jwt/v4"
 	"github.com/06202003/apiInventory/helper"
+	"github.com/golang-jwt/jwt/v4"
 	"gorm.io/gorm"
 
 	"github.com/06202003/apiInventory/models"
 	"golang.org/x/crypto/bcrypt"
 )
+
+// func Login(w http.ResponseWriter, r *http.Request) {
+// 	// mengambil inputan json
+// 	var userInput models.User
+// 	decoder := json.NewDecoder(r.Body)
+// 	if err := decoder.Decode(&userInput); err != nil {
+// 		response := map[string]string{"message": err.Error()}
+// 		helper.ResponseJSON(w, http.StatusBadRequest, response)
+// 		return
+// 	}
+// 	defer r.Body.Close()
+
+// 	// ambil data user berdasarkan Email
+// 	var user models.User
+// 	if err := models.DB.Where("Email = ?", userInput.Email).First(&user).Error; err != nil {
+// 		switch err {
+// 		case gorm.ErrRecordNotFound:
+// 			response := map[string]string{"message": "Email atau password salah"}
+// 			helper.ResponseJSON(w, http.StatusUnauthorized, response)
+// 			return
+// 		default:
+// 			response := map[string]string{"message": err.Error()}
+// 			helper.ResponseJSON(w, http.StatusInternalServerError, response)
+// 			return
+// 		}
+// 	}
+
+// 	// cek apakah password valid
+// 	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(userInput.Password)); err != nil {
+// 		response := map[string]string{"message": "Email atau password salah"}
+// 		helper.ResponseJSON(w, http.StatusUnauthorized, response)
+// 		return
+// 	}
+
+//     // proses pembuatan token jwt
+//     expTime := time.Now().Add(24 * time.Hour) // Set expiration to 24 hours
+//     claims := &config.JWTClaim{
+//         Email: user.Email,
+//         StandardClaims: jwt.StandardClaims{
+//             Issuer:    "go-jwt-mux",
+//             ExpiresAt: expTime.Unix(),
+//         },
+//     }
+
+//     // medeklarasikan algoritma yang akan digunakan untuk signing
+//     tokenAlgo := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+//     // signed token
+//     token, err := tokenAlgo.SignedString(config.JWT_KEY)
+//     if err != nil {
+//         response := map[string]string{"message": err.Error()}
+//         helper.ResponseJSON(w, http.StatusInternalServerError, response)
+//         return
+//     }
+
+//     // set token ke cookie
+//     http.SetCookie(w, &http.Cookie{
+//         Name:     "token",
+//         Path:     "/",
+//         Value:    token,
+//         HttpOnly: true,
+// 		MaxAge:   24 * 60 * 60,
+//     })
+
+//     response := map[string]string{"message": "login berhasil"}
+//     helper.ResponseJSON(w, http.StatusOK, response)
+// }
 
 func Login(w http.ResponseWriter, r *http.Request) {
 	// mengambil inputan json
@@ -41,48 +107,54 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	// cek apakah password valid
-	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(userInput.Password)); err != nil {
+	// retrieve the static salt from the user model
+	staticSalt := "WITAsik"
+
+	// combine entered password and salt
+	passwordWithSalt := userInput.Password + staticSalt
+
+	// verify if the entered password matches the stored hashed password
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(passwordWithSalt)); err != nil {
 		response := map[string]string{"message": "Email atau password salah"}
 		helper.ResponseJSON(w, http.StatusUnauthorized, response)
 		return
 	}
 
-    // proses pembuatan token jwt
-    expTime := time.Now().Add(24 * time.Hour) // Set expiration to 24 hours
-    claims := &config.JWTClaim{
-        Email: user.Email,
-        StandardClaims: jwt.StandardClaims{
-            Issuer:    "go-jwt-mux",
-            ExpiresAt: expTime.Unix(),
-        },
-    }
+	// proses pembuatan token jwt
+	expTime := time.Now().Add(24 * time.Hour) // Set expiration to 24 hours
+	claims := &config.JWTClaim{
+		Email: user.Email,
+		StandardClaims: jwt.StandardClaims{
+			Issuer:    "go-jwt-mux",
+			ExpiresAt: expTime.Unix(),
+		},
+	}
 
-    // medeklarasikan algoritma yang akan digunakan untuk signing
-    tokenAlgo := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-    // signed token
-    token, err := tokenAlgo.SignedString(config.JWT_KEY)
-    if err != nil {
-        response := map[string]string{"message": err.Error()}
-        helper.ResponseJSON(w, http.StatusInternalServerError, response)
-        return
-    }
+	// medeklarasikan algoritma yang akan digunakan untuk signing
+	tokenAlgo := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	// signed token
+	token, err := tokenAlgo.SignedString(config.JWT_KEY)
+	if err != nil {
+		response := map[string]string{"message": err.Error()}
+		helper.ResponseJSON(w, http.StatusInternalServerError, response)
+		return
+	}
 
-    // set token ke cookie
-    http.SetCookie(w, &http.Cookie{
-        Name:     "token",
-        Path:     "/",
-        Value:    token,
-        HttpOnly: true,
+	// set token ke cookie
+	http.SetCookie(w, &http.Cookie{
+		Name:     "token",
+		Path:     "/",
+		Value:    token,
+		HttpOnly: true,
 		MaxAge:   24 * 60 * 60,
-    })
+	})
 
-    response := map[string]string{"message": "login berhasil"}
-    helper.ResponseJSON(w, http.StatusOK, response)
+	response := map[string]string{"message": "login berhasil"}
+	helper.ResponseJSON(w, http.StatusOK, response)
 }
 
-func Register(w http.ResponseWriter, r *http.Request) {
 
+func Register(w http.ResponseWriter, r *http.Request) {
 	// mengambil inputan json
 	var userInput models.User
 	decoder := json.NewDecoder(r.Body)
@@ -93,11 +165,24 @@ func Register(w http.ResponseWriter, r *http.Request) {
 	}
 	defer r.Body.Close()
 
-	// hash pass menggunakan bcrypt
-	hashPassword, _ := bcrypt.GenerateFromPassword([]byte(userInput.Password), bcrypt.DefaultCost)
+	// static salt
+	staticSalt := "WITAsik"
+
+	// combine password and salt
+	passwordWithSalt := userInput.Password + staticSalt
+
+	// hash pass using bcrypt
+	hashPassword, err := bcrypt.GenerateFromPassword([]byte(passwordWithSalt), bcrypt.DefaultCost)
+	if err != nil {
+		response := map[string]string{"message": "Error hashing password"}
+		helper.ResponseJSON(w, http.StatusInternalServerError, response)
+		return
+	}
+
+	// update password with hashed password
 	userInput.Password = string(hashPassword)
 
-	// insert ke database
+	// insert into the database
 	if err := models.DB.Create(&userInput).Error; err != nil {
 		response := map[string]string{"message": err.Error()}
 		helper.ResponseJSON(w, http.StatusInternalServerError, response)
@@ -107,6 +192,7 @@ func Register(w http.ResponseWriter, r *http.Request) {
 	response := map[string]string{"message": "success"}
 	helper.ResponseJSON(w, http.StatusOK, response)
 }
+
 
 func Logout(w http.ResponseWriter, r *http.Request) {
 	// hapus token yang ada di cookie
